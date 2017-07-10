948d5351d8661c4d25be3fd7e5ed60ae6727eaf5
michael <michael@michael-VirtualBox>
2017-07-10 19:25:08 +0200

    tests

--- src/main/java/testgroup/testartifact/App.java
+++ src/main/java/testgroup/testartifact/App.java
@@ -1,10 +1,18 @@
 package testgroup.testartifact;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
 
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.google.common.base.Stopwatch;
 
 
 /**
@@ -13,7 +21,7 @@ import com.datastax.driver.core.Session;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!!!" );
         
@@ -25,25 +33,48 @@ public class App
         		.build();
         Session session = cluster.connect(keyspace);
         
-        String cqlStatement = "SELECT json * FROM testdata limit 10;";
+        
+
+        
+        String cqlStatement = "SELECT * FROM testdata limit 10;";
         
         // read all entries from users table
         for(Row row : session.execute(cqlStatement)) {
-        	System.out.println(row.toString());
+        	System.out.println(row.getString("txt"));
+        	
+        	Map<String,String> themap = row.getMap("something", String.class, String.class);
+        	System.out.println("size: "+themap.size());
+        	System.out.println("a="+themap.get("a"));
+        	System.out.println("b="+themap.get("b"));
         }
         
+        
+        /*
+        
+        BatchStatement batch = new BatchStatement(BatchStatement.Type.UNLOGGED);
+        PreparedStatement prep = session.prepare("INSERT INTO users (id, name) VALUES (?, ?)");
+        long starttime = System.nanoTime();
+        
+        // add new users
+        for(int i = 1; i <= 100; i++) {
+	        BoundStatement bound = new BoundStatement(prep);
+	        bound.bind("'user"+i+"'", "'name"+i+"'");
+	        batch.add(bound);
+        }
+        session.execute(batch);
+        long endtime = System.nanoTime();
+        System.out.println("Duration (ms): "+ (endtime-starttime)/1000000);
+        */
         
         
         long starttime = System.nanoTime();
-        /*
-        // add new users
-        for(int i = 1; i <= 10000; i++) {
-	        String cqlInsertStatement = "INSERT INTO users (id, name) "
-	        		+ "VALUES ('user"+i+"', 'john"+i+"');";
-	
-	        session.execute(cqlInsertStatement);
+        PreparedStatement prep = session.prepare("INSERT INTO users (id, name) VALUES (?, ?)");
+        for(int i = 1; i <= 1000; i++) {
+	        BoundStatement bound = new BoundStatement(prep);
+	        bound.bind("'benutzer"+i+"'", "'name"+i+"'");
+	        session.executeAsync(bound);
         }
-        */
+        
         long endtime = System.nanoTime();
         System.out.println("Duration (ms): "+ (endtime-starttime)/1000000);
         
@@ -53,10 +84,22 @@ public class App
        // }
         
         
+
+        
         System.out.println("Done");
         session.close();
         cluster.close();
         System.exit(0);
         
     }
+    
+    
+    
+
+
+    
+    
+    
+    
+    
 }
